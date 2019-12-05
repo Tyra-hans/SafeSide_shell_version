8fd5da83ac80a53a17029d7cb068696031559c65
Tyra-hans <tyrahans17@gmail.com>
2019-12-05 16:01:38 +0300

    Creates a condition to check whether there are alerts in the location

--- weather.py
+++ weather.py
@@ -1,6 +1,37 @@
 import requests
 from pprint import pprint
 
+print('''
+
+          *******                  ***                      *******                 **              
+    *       ***              ** ***                   *       ***    *           **             
+   *         **             **   ***                 *         **   ***          **             
+   **        *              **                       **        *     *           **             
+    ***                     **                        ***                        **             
+   ** ***           ****    ******      ***          ** ***        ***       *** **      ***    
+    *** ***        * ***  * *****      * ***          *** ***       ***     *********   * ***   
+      *** ***     *   ****  **        *   ***           *** ***      **    **   ****   *   ***  
+        *** ***  **    **   **       **    ***            *** ***    **    **    **   **    *** 
+          ** *** **    **   **       ********               ** ***   **    **    **   ********  
+           ** ** **    **   **       *******                 ** **   **    **    **   *******   
+            * *  **    **   **       **                       * *    **    **    **   **        
+  ***        *   **    **   **       ****    *      ***        *     **    **    **   ****    * 
+ *  *********     ***** **  **        *******      *  *********      *** *  *****      *******  
+*     *****        ***   **  **        *****      *     *****         ***    ***        *****   
+*                                                 *                                             
+ **                                                **                                           
+                                                                                                
+                                                        
+    ''')
+
+print('==============================================================================================')
+print('=============================Welcome To SafeSide==============================================')
+print('==================Weather alerts to help keep you on the safe side============================')
+print('==============================================================================================')
+print('''
+
+
+''')
 city = input('Enter your city: ')
 
 url='https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name={}&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg'.format(city)
@@ -11,4 +42,10 @@ data = res.json()
 
 pprint(data)
 
-print('alert :' , data['alerts']['alerts'][0]['description'],'. Stay safe')
+if data['alerts']['alerts'] == []:
+    print('Nothing to worry about No extreem weather at ', city)
+
+else :
+    print('alert :' , data['alerts']['alerts'][0]['description'],'. Stay safe')
+    
+
