7c32f7c23f0def93a7b923597f1ded7bc9f9e924
Tyra-hans <tyrahans17@gmail.com>
2019-12-05 16:11:55 +0300

    adds a second api url and displays actual weather conditions of any city in the country

--- weather.py
+++ weather.py
@@ -36,16 +36,33 @@ city = input('Enter your city: ')
 
 url='https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name={}&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg'.format(city)
 
+url2='https://openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'.format(city)
+
 res = requests.get(url)
+
+res2 = requests.get(url2)
 
 data = res.json()
 
-pprint(data)
+data2 = res2.json()
+
+# pprint(data)
+
+pprint(data2)
 
 if data['alerts']['alerts'] == []:
     print('Nothing to worry about No extreem weather at ', city)
 
 else :
     print('alert :' , data['alerts']['alerts'][0]['description'],'. Stay safe')
+
+print('Here is the weather in ' , city)
+
+humidity = data2['main']['humidity']
+temp = data2['mai']
+
+print('humidity :', humidity)
+
+
     
 
