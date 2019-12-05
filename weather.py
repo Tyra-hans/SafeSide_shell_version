a2d6f3c21487844889bda8cf95dc6d849e2704e6
Tyra-hans <tyrahans17@gmail.com>
2019-12-05 15:43:03 +0300

    Adds api endpoint to allow users to query for data per location

---
+++ weather.py
@@ -0,0 +1,12 @@
+import requests
+
+
+city = input('Enter your city: ')
+
+url='https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name={}&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg'.format(city)
+
+res = requests.get(url)
+
+data = res.json()
+
+print('alert :' , data['alerts']['alerts'][0]['description'],'. Stay safe')
